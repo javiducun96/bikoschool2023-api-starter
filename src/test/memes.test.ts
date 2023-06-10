bf6cc9babd08442e200fc51eeec8bbb417c0c2b4
javiducun <javiducun@gmail.com>
2023-06-10 11:24:21 +0200

    test: check if memes is an array

--- src/test/memes.test.ts
+++ src/test/memes.test.ts
@@ -1,15 +1,25 @@
+import { response } from "express"
 import app from "../app"
 import request from "supertest"
 
-describe("GET /user", function () {
-  it("memes endpoint exists", function (done) {
+describe("Memes endpoint", function () {
+  it("exists", function (done) {
     request(app).get("/api/memes").expect(200, done)
   })
 
-  it("memes endpoint return json", function (done) {
+  it("return json", function (done) {
     request(app)
       .get("/api/memes")
       .expect("Content-Type", /application\/json/)
       .expect(200, done)
+  })
+
+  it("return array json", function (done) {
+    request(app)
+      .get("/api/memes")
+      .expect("Content-Type", /application\/json/)
+      .then((response) => {
+        expect(Array.isArray(response.body)).toBeTruthy()
+      })
   })
 })
