3e5415e0ce2c930ddaeba394ed75a66ccfb93da6
javiducun <javiducun@gmail.com>
2023-06-10 13:52:24 +0200

    test: use createApp instead of deprecated import app

--- src/test/memes.test.ts
+++ src/test/memes.test.ts
@@ -1,8 +1,12 @@
 import { response } from "express"
-import app from "../app"
+import { createApp } from "../app"
+import { createDB } from "../services/dbService"
 import request from "supertest"
 
 describe("Memes endpoint", function () {
+  const dbPath: string = process.env.DBPATH || "./data/db.json"
+  const db = createDB(dbPath)
+  const app = createApp(db)
   it("exists", async () => await request(app).get("/api/memes").expect(200))
 
   it("return json", async () =>
