b8d27364b791ef9ce03258913ce8e8c1c662766d
javiducun <javiducun@gmail.com>
2023-06-10 14:23:31 +0200

    test: not use morgan on test

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import createMemesRouter from "./routes/memes"
 
 export function createApp(db: LowdbSync<DatabaseSchema>) {
   const app: Express = express()
-  app.use(morgan("dev"))
+  if (process.env.NODE_ENV !== "test") app.use(morgan("dev"))
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
   createRoutes(app, db)
