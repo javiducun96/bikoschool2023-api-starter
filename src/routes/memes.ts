7aae2fa674536c7742c0b737b34e3c0384c616f1
javiducun <javiducun@gmail.com>
2023-06-10 13:12:55 +0200

    feat: return 50 memes from lowdb

--- src/routes/memes.ts
+++ src/routes/memes.ts
@@ -1,9 +1,21 @@
 import express, { Express, Router, Request, Response } from "express"
+import low from "lowdb"
+import FileSync from "lowdb/adapters/FileSync"
+
+interface DatabaseSchema {
+  memes: Meme[]
+}
+
+interface Meme {}
 
 const router: Router = express.Router()
 router.get("/", async (req: Request, res: Response) => {
+  const adapter = new FileSync<DatabaseSchema>("./data/db.json")
+  const db = low(adapter)
+  const response = db.get("memes").take(50).value()
+
   res.setHeader("Content-Type", "application/json")
-  res.json([])
+  res.json(response)
 })
 
 export default router
