ad99399bb9e3dbb9e6a0b82d7d2a4230e3f48d24
javiducun <javiducun@gmail.com>
2023-06-10 10:29:26 +0200

    add test: memes endpoint exists

---
+++ src/test/memes.test.ts
@@ -0,0 +1,8 @@
+import app from "../app"
+import request from "supertest"
+
+describe("GET /user", function () {
+  it("memes endpoint exists", function (done) {
+    request(app).get("/memes").expect(200, done)
+  })
+})
