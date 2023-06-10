89aa730d6f229ac65a98bf85aa8ca455ac750ccb
javiducun <javiducun@gmail.com>
2023-06-10 11:40:58 +0200

    refactor: use json funcion of express

--- src/routes/memes.ts
+++ src/routes/memes.ts
@@ -3,7 +3,7 @@ import express, { Express, Router, Request, Response } from "express"
 const router: Router = express.Router()
 router.get("/", async (req: Request, res: Response) => {
   res.setHeader("Content-Type", "application/json")
-  res.send(JSON.stringify([{ a: 1 }]))
+  res.json([])
 })
 
 export default router
